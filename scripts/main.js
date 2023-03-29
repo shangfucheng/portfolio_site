79d7dfbd623508edd3901d6a57e1c7420dc97e94
shangfucheng <shangfucheng1@hotmail.com>
2023-03-28 22:16:09 -0700

    update about me

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,16 @@ window.addEventListener("DOMContentLoaded", init);
 
 // Starts the program, all function calls trace back here
 function init() {
-    let funCards = document.getElementById('fun_cards');
-    funCards.addEventListener('mouseover', (event)=>{
-        windowWidth = 800;
-        windowHeight = 800;
-        mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
-        mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+    /*Fun card background*/
+    // let funCards = document.getElementById('fun_cards');
+    // funCards.addEventListener('mouseover', (event)=>{
+    //     windowWidth = 800;
+    //     windowHeight = 800;
+    //     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    //     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
         
-        document.getElementById('fun_cards').style.background = ('radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #2a2b29)');
-    });
+    //     document.getElementById('fun_cards').style.background = ('radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #2a2b29)');
+    // });
     let cards = document.getElementsByClassName('card');
     Array.from(cards).forEach((c)=>{
         c.addEventListener('mouseover', (event)=>{
@@ -18,7 +19,7 @@ function init() {
             windowHeight = 260;
             mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
             mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-            c.style.background = ('radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #00f7ef, #2a2b29)');
+            c.style.background = ('radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #66adad, #535e5e)');
 
             let text = c.querySelectorAll('p');
             Array.from(text).forEach((txt)=>{
